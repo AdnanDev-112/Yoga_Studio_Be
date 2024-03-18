91df03af8be0fa8d9c16352b01b55422139bb03e
skadam62 <ccieshailendra@gmail.com>
2024-03-18 15:27:33 +0000

    schedule page

--- src/main/java/com/enterprise/YogaStudio/controller/ScheduleController.java
+++ src/main/java/com/enterprise/YogaStudio/controller/ScheduleController.java
@@ -1,30 +1,42 @@
 package com.enterprise.YogaStudio.controller;
-
-import com.enterprise.YogaStudio.dto.ScheduleDTO;
-import com.enterprise.YogaStudio.model.Client;
 import com.enterprise.YogaStudio.model.Schedule;
-import com.enterprise.YogaStudio.service.ClientService;
 import com.enterprise.YogaStudio.service.ScheduleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
 @RequestMapping("/schedule")
-@CrossOrigin(origins = "http://localhost:3001")
+@CrossOrigin(origins = "http://localhost:3000")
 public class ScheduleController {
 
     @Autowired
     private ScheduleService scheduleService;
 
-    @GetMapping("/getschedulelist")
-    public ResponseEntity<List<ScheduleDTO>> getScheduleList() {
-        List<ScheduleDTO> schedule = scheduleService.getScheduleList();
-        return ResponseEntity.ok(schedule);
+    @GetMapping("/getschedulebycategory")
+    public ResponseEntity<List<Schedule>> getBookingByCategory(@RequestParam String categoryType, @RequestParam String clientID) {
+
+    List<Schedule> schedules = scheduleService.getBookingsByCategoryType(categoryType, clientID);
+
+
+    return ResponseEntity.ok(schedules);
     }
+
+
+    @GetMapping("/getschedulebycategorytype")
+    public ResponseEntity<List<Schedule>> getScheduleByCategoryType(@RequestParam String categoryType) {
+
+        List<Schedule> schedules = scheduleService.getScheduleByCategoryType(categoryType);
+
+
+        return ResponseEntity.ok(schedules);
+    }
+
+
+    @PostMapping("/addnewscheduleentry")
+    public Schedule addNewScheduleEntry(@RequestBody Schedule newEntry) {
+        return scheduleService.addNewScheduleEntry(newEntry);
+    }
+
 }
--- src/main/java/com/enterprise/YogaStudio/service/ScheduleService.java
+++ src/main/java/com/enterprise/YogaStudio/service/ScheduleService.java
@@ -1,7 +1,6 @@
 package com.enterprise.YogaStudio.service;
 
-import com.enterprise.YogaStudio.dto.ScheduleDTO;
-import com.enterprise.YogaStudio.model.Client;
+
 import com.enterprise.YogaStudio.model.Schedule;
 import org.springframework.stereotype.Service;
 
@@ -10,5 +9,11 @@ import java.util.List;
 @Service
 public interface ScheduleService {
 
-    List<ScheduleDTO> getScheduleList();
+
+
+    public List<Schedule> getBookingsByCategoryType(String categoryType, String clientID);
+
+    List<Schedule> getScheduleByCategoryType(String categoryType);
+
+    Schedule addNewScheduleEntry(Schedule newEntry);
 }

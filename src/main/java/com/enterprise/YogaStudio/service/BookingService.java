c273109be15b28cfa4b4793645b15245fabfcce4
skadam62 <ccieshailendra@gmail.com>
2024-03-18 21:27:38 +0000

    fixed the error

--- src/main/java/com/enterprise/YogaStudio/service/BookingService.java
+++ src/main/java/com/enterprise/YogaStudio/service/BookingService.java
@@ -18,7 +18,7 @@ public interface BookingService {
 
     List<Booking> getBookingsByClientId(Integer clientId);
 
-   // Booking addBooking(AddBookingDTO bookingData);
+   Booking addBooking(AddBookingDTO bookingData);
 
   //  void addSchedule(ScheduleRequest request);
 }
